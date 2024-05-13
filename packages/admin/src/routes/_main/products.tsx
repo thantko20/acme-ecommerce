3eb6a73fa56414f758f779b1ce88a016ffdf3648
Thant Ko Zaw <thantmkou10@gmail.com>
2024-05-13 21:46:34 +0630

    changes

--- packages/admin/src/routes/_main/products.tsx
+++ packages/admin/src/routes/_main/products.tsx
@@ -1,5 +1,106 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const Route = createFileRoute("/_main/products")({
-  component: () => <div>Hello /_main/products!</div>,
+  component: () => <ProductsList />,
+  loader: ({ context }) => {
+    return context.client.product.list.query();
+  },
 });
+
+export default function ProductsList() {
+  const { data: products } = Route.useLoaderData();
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-bold">Products</h2>
+        <Button>Add Product</Button>
+      </div>
+      <Card className="mt-4">
+        <CardContent className="pt-4">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="hidden w-[100px] sm:table-cell">
+                  <span className="sr-only">img</span>
+                </TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead>Slug</TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Created at
+                </TableHead>
+                <TableHead>
+                  <span className="sr-only">Actions</span>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {products.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell className="hidden sm:table-cell">
+                    <img
+                      alt="Product img"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="/vite.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">{product.name}</TableCell>
+                  <TableCell>{product.description}</TableCell>
+                  <TableCell>{product.slug}</TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    {new Date(product.createdAt).toLocaleString()}
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <div className="text-xs text-muted-foreground">
+            Showing <strong>1-10</strong> of <strong>32</strong> products
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- packages/server/src/trpc/index.ts
+++ packages/server/src/trpc/index.ts
@@ -1,10 +1,12 @@
 import { authRouter } from "./routers/auth";
+import { productsRouter } from "./routers/products";
 import { usersRouter } from "./routers/users";
 import { router } from "./trpc";
 
 export const appRouter = router({
   user: usersRouter,
   auth: authRouter,
+  product: productsRouter,
 });
 
 export type AppRouter = typeof appRouter;
